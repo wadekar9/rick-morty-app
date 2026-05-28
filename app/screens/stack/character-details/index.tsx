c1d6448b1bd383603420fed4bdfb8bd15dbed16d
wadekar9 <npwadekar7@gmail.com>
2026-05-29 00:34:13 +0530

    feat: implement character details screen UI

--- app/screens/stack/character-details/index.tsx
+++ app/screens/stack/character-details/index.tsx
@@ -1,14 +1,96 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { ThemedView, ThemeText } from '$components/ui';
-import { AppStackScreenProps } from '$types/navigation.types';
-import { EStackScreens } from '$constants/screen.constants';
+import { View } from 'react-native'
+import React from 'react'
+import { AppStackScreenProps } from '$types/navigation.types'
+import { EStackScreens } from '$constants/screen.constants'
+import { BaseImage, IconButton, ThemedView, ThemeText } from '$components/ui'
+import { useAppTheme } from '$hooks/common'
+import { moderateScale } from '$constants/styles.constants'
+import { styling } from './styles'
+import { ScrollView } from 'react-native-gesture-handler'
+import { ChevronLeft, Heart } from 'lucide-react-native'
+import { StatusBadge } from '$components/layout'
 
-const CharacterDetails: React.FC<AppStackScreenProps<EStackScreens.CHARACTER_DETAIL>> = () => {
+const CharacterDetails: React.FC<AppStackScreenProps<EStackScreens.CHARACTER_DETAIL>> = ({ navigation }) => {
+
+    const { colors, theme, insets } = useAppTheme();
+    const styles = styling(theme, insets);
 
     return (
         <ThemedView>
-            <ThemeText>Character Details</ThemeText>
+            <ScrollView
+                contentContainerStyle={styles.contentContainer}
+                showsVerticalScrollIndicator={false}
+                bounces={false}
+                overScrollMode='never'
+            >
+                <View style={styles.image}>
+                    <BaseImage
+                        wrapperStyle={{ width: '100%', height: '100%' }}
+                        source={{ uri: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' }}
+                        resizeMode={'cover'}
+                    />
+
+                    <View style={styles.statusWrapper}>
+                        <StatusBadge theme={theme} status='Alive' />
+                    </View>
+                </View>
+
+                <View style={styles.content}>
+                    <ThemeText numberOfLines={2} style={styles.title}>{'Rick Sanchez'}</ThemeText>
+
+                    <View style={styles.flexRow}>
+                        <ThemeText style={styles.keyLabel}>Species: </ThemeText>
+                        <ThemeText style={styles.keyValue}>Human</ThemeText>
+                    </View>
+                    <View style={styles.flexRow}>
+                        <ThemeText style={styles.keyLabel}>Gender: </ThemeText>
+                        <ThemeText style={styles.keyValue}>Male</ThemeText>
+                    </View>
+                    <View style={styles.flexRow}>
+                        <ThemeText style={styles.keyLabel}>Origin: </ThemeText>
+                        <ThemeText style={styles.keyValue}>Earth (C-137)</ThemeText>
+                    </View>
+                    <View style={styles.flexRow}>
+                        <ThemeText style={styles.keyLabel}>Location: </ThemeText>
+                        <ThemeText style={styles.keyValue}>Citadel of Ricks</ThemeText>
+                    </View>
+
+                    <View style={styles.section}>
+                        <ThemeText variant='h3'>Episodes:</ThemeText>
+
+                        <View style={styles.episodes}>
+                            {Array.from({ length: 20 }, (_, idx) => {
+
+                                const episodeNumber = 'https://rickandmortyapi.com/api/episode/1'.split('/').pop();
+
+                                return (
+                                    <View
+                                        key={`${idx}`}
+                                        style={styles.episode}
+                                    >
+                                        <ThemeText variant='body2'>{`E${episodeNumber}`}</ThemeText>
+                                    </View>
+                                );
+                            })}
+                        </View>
+                    </View>
+                </View>
+
+
+                <View style={styles.headerActions}>
+                    <IconButton
+                        style={styles.headerAction}
+                        onPress={() => navigation.goBack()}
+                    >
+                        <ChevronLeft width={moderateScale(24)} height={moderateScale(24)} color={colors['text-primary']} />
+                    </IconButton>
+                    <IconButton
+                        style={styles.headerAction}
+                    >
+                        <Heart width={moderateScale(24)} height={moderateScale(24)} color={colors['text-primary']} />
+                    </IconButton>
+                </View>
+            </ScrollView>
         </ThemedView>
     );
 };
--- app/screens/stack/character-details/styles.ts
+++ app/screens/stack/character-details/styles.ts
@@ -1,10 +1,89 @@
-import { COLORS } from '$constants/colors.constants';
-import { ITheme } from '$types/theme.types';;
-import { StyleSheet } from 'react-native';
+import { COLORS } from "$constants/colors.constants";
+import { DEVICE_WIDTH, EFonts, EFontSize, moderateScale } from "$constants/styles.constants";
+import { ITheme } from "$types/theme.types";
+import { StyleSheet } from "react-native";
+import { EdgeInsets } from "react-native-safe-area-context";
 
-export const styling = (theme: ITheme) => StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS[theme].background,
+export const styling = (theme: ITheme, insets: EdgeInsets) => StyleSheet.create({
+    image: {
+        width: DEVICE_WIDTH,
+        height: undefined,
+        aspectRatio: 1.25,
+        borderBottomRightRadius: moderateScale(20),
+        borderBottomLeftRadius: moderateScale(20),
+        overflow: 'hidden'
     },
+    contentContainer: {
+        flexGrow: 1
+    },
+    content: {
+        padding: moderateScale(16),
+        paddingBottom: insets.bottom + moderateScale(16),
+        gap: moderateScale(10),
+    },
+    title: {
+        fontSize: EFontSize["3XL"] - 2,
+        fontFamily: EFonts.SEMI_BOLD,
+        color: COLORS[theme]["text-primary"]
+    },
+    flexRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: moderateScale(8),
+        flexWrap: 'wrap',
+        width: '100%'
+    },
+    keyLabel: {
+        fontSize: EFontSize.XL,
+        fontFamily: EFonts.MEDIUM,
+        color: COLORS[theme]["text-primary"]
+    },
+    keyValue: {
+        fontSize: EFontSize.XL,
+        fontFamily: EFonts.REGULAR,
+        color: COLORS[theme]["text-secondary"],
+        letterSpacing: 0.15
+    },
+    headerActions: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: moderateScale(16),
+        gap: moderateScale(10),
+        width: '100%',
+        position: 'absolute',
+        top: 0
+    },
+    headerAction: {
+        width: moderateScale(50),
+        height: moderateScale(50),
+        borderRadius: moderateScale(50),
+        backgroundColor: COLORS[theme]["surface-alt"],
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    statusWrapper: {
+        position: 'absolute',
+        bottom: moderateScale(16),
+        right: moderateScale(16)
+    },
+    section: {
+        gap: moderateScale(16),
+        paddingVertical: moderateScale(10)
+    },
+    episodes: {
+        width: '100%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: moderateScale(10)
+    },
+    episode: {
+        width: (DEVICE_WIDTH - moderateScale(55)) / 3,
+        height: moderateScale(50),
+        borderRadius: moderateScale(4),
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: COLORS[theme].border
+    }
 });
